bf13059736f8ea5868d932af178f83c3f42abae3
Nsix6 <nsikakmbre@gmail.com>
2025-10-24 12:40:29 +0100

    gives description of a table

--- task_4.sql
+++ task_4.sql
@@ -1,1 +1,4 @@
-SHOW COLUMN FROM alx_book_store;
+SELECT column_name, data_type, is_nullable, column_default
+FROM information_schema.columns
+WHERE table_schema = 'alx_book_store'
+  AND table_name = 'books';
